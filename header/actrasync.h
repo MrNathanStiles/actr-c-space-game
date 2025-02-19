cdae877080b464bafd1534daa461828ccacab993
Nathan Stiles <stiles.nathan@outlook.com>
2025-02-19 12:59:53 -0500

    work

--- header/actrasync.h
+++
@@ -1,8 +0,0 @@
-#ifndef ACTRASYNC_H
-#define ACTRASYNC_H
-enum AsyncResult {
-    AsyncResultSuccess = 1,
-    AsyncResultFailure = 0,
-    AsyncResultAPIError = -1
-};
-#endif
--- header/actrprng.h
+++
@@ -1,6 +0,0 @@
-#ifndef ACTRPRNG_H
-#define ACTRPRNG_H
-#include "actrtime.h"
-
-    extern float actr_prng();
-#endif
